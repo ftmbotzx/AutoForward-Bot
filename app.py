6e144bd3f309b4195c8c30ac404e4c9e1ea4c6b3
ftmbotzx <kumarasta91@gmail.com>
2025-08-13 14:24:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import asyncio
 
 # --- CONFIG ---
 MONGO_URI = "mongodb+srv://ftmbotzx:ftmbotzx@cluster0.0b8imks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "forwardDB"
-PROGRESS_COLLECTION = "ftmdb1"
+DB_NAME = "ftmdb"
+PROGRESS_COLLECTION = "ftmdb"
 FORWARDED_COLLECTION = "forwarded_files"
 
 # --- Flask App ---
