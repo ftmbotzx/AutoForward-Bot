b6fde22c9f7cf6ee6844e9cda60f1eb6b0e8f4b9
ftmbotzx <kumarasta91@gmail.com>
2025-08-19 09:54:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ progress_col = db[COLLECTION_NAME]
 # Telethon Client setup
 api_id = int(os.getenv("API_ID", "8012239"))
 api_hash = os.getenv("API_HASH", "171e6f1bf66ed8dcc5140fbe827b6b08")
-session_string = os.getenv("SESSION_STRING", "1BVtsOIsBuz87kY2XOsjO2cVFnHJQoweOxT1UHxPcMD0INENo4rIyVNWmut-GUozCk0m5--J9HehX7Vg_cvPWrapE9x5hbSyfBVPRAJSAB2Y1iVNJGjyAvpNCWYumzG2Np4adB-AUKboLFjjWTKsKS9r8NBEes3mdDNdpV63LICOGMnqfSjJ2DYd51luISvVFU-D61GgaL3Ig8z4Pl05qx7eoYrBumtCJKqPjpSEQpuP5S4Ch1QVMFozp8FjpQkp4XnTaNXkOjH64FTU-GCQRcaSqKUFfIaXJEtrH_sbC06osxHuk4OoDh4v0cDV_7ASWoW11KvTBM7uc2IOJ8-6OM4SzXukh0MU=")
+session_string = os.getenv("SESSION_STRING", "1BVtsOIIBuy0Pghyup2lOE1yDjXg-mRoTWpCmQ-jxDqBvJXhD4leCcMdVKouFgkWE33a3EH1DZepwVKs8eaPkL3N9uSl_nG469aP94TeoP5FOqcXIvAIXbndK_wmgGkC0UwPHE8enIu8AQogIcwSdf9cbs5Lk1xq2OuLF4uEgXTwulRRrUXlVY0SaSy6T4LZcRPB1EEYLSRDl1-i0Yw8Pg22C8ktSVzGZHovzt2rh51C7BUhv1QaNYqjVTVtDDPo6HO71qyQ2MGOlU7s-gadC_9VcqhrfAgHbRMvuKNnz-2lh2ESuWCxtuTwThHVBZdbSwrLatCyBBc4cBR1ZEkYl2-qUEQckVjQ=")
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
